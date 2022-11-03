bf70db9294af052dc1c7402c43694f3e18e97872
Haesol <osunpine@gmail.com>
2022-11-04 00:32:07 +0900

    chore. 프로젝트 초기 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Header} from "./components/common/Header";
+import {Route, Routes} from "react-router-dom";
+import {LogInPage} from "./pages/LogInPage";
+import {SignUpPage} from "./pages/SignUpPage";
+import {FindIdPage} from "./pages/FindIdPage";
+import {FindPwdPage} from "./pages/FindPwdPage";
+import {MainPage} from "./pages/MainPage";
+import {OmokPage} from "./pages/OmokPage";
+import {EndingPage} from "./pages/EndingPage";
+import {UserPage} from "./pages/UserPage";
+import {Footer} from "./components/common/Footer";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Routes>
+                <Route path={"/"} element={<LogInPage/>}/>
+                <Route path={"/sign-up"} element={<SignUpPage/>}/>
+                <Route path={"/find-id"} element={<FindIdPage/>}/>
+                <Route path={"/find-pwd"} element={<FindPwdPage/>}/>
+                <Route path={"/main"} element={<MainPage/>}/>
+                <Route path={"/omok/:roomId"} element={<OmokPage/>}/>
+                <Route path={"/ending"} element={<EndingPage/>}/>
+                <Route path={"/user"} element={<UserPage/>}/>
+            </Routes>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
