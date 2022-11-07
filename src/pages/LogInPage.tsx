c0765299b39f5de4ca911e9f5de80bda6c3225ce
Haesol <osunpine@gmail.com>
2022-11-07 20:14:56 +0900

    feat. 공통 컴포넌트로 변경

--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import styles from "./styles/logInPage.module.scss"
-import logo from "../assets/logo/logo.svg"
+import React from "react";
+import styles from "./styles/logInPage.module.scss";
+import LogoTitle from "../components/common/LogoTitle";
 import LogInForm from "../components/logInPage/LogInForm";
 import LogInButtons from "../components/logInPage/LogInButtons";
 
 const LogInPage = () => {
-    return (
-        <div className={styles.container}>
-            <img src={logo} alt="logo" className={styles.logo}/>
-            <p className={styles.title}>실시간 일대일 오목 게임, 오목캣</p>
-            <LogInForm/>
-            <LogInButtons/>
-        </div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <LogoTitle title={"실시간 일대일 오목 게임, 오목캣"} />
+      <LogInForm />
+      <LogInButtons />
+    </div>
+  );
+};
 
-export default LogInPage
+export default LogInPage;
