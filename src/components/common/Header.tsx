7072779501c58cd404486a29b5d0168f7e32968f
seulgiJ <qlenf1560@gmail.com>
2022-11-07 16:45:28 +0900

    feat. 헤더 스타일 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import logoBlack from '../../assets/logo/logo_black.svg'
-import profile from '../../assets/icons/default_profile.svg'
-import '../../shared/styles/header.scss'
+import React from "react";
+import logoBlack from "../../assets/logo/logo_black.svg";
+import profile from "../../assets/icons/default_profile.svg";
+import "../../shared/styles/header.scss";
 
 const Header = () => {
-    return (
-        <div className='headerWrapper'>
-            <img className='headerLogo' src={logoBlack} alt="logo"/>
-            <p>귀여운 고양이들과 함께 하는 오목 한 판!</p>
-            <img className='headerProfile' src={profile} alt="profile"/>
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <div className="headerWrapper">
+        <img className="headerLogo" src={logoBlack} alt="logo" />
+        <p>귀여운 고양이들과 함께 하는 오목 한 판!</p>
+        <img className="headerProfile" src={profile} alt="profile" />
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
