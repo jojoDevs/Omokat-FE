7e9c7e1eefea1a483b19d721be922cf715c28e52
Haesol <osunpine@gmail.com>
2022-11-07 16:44:13 +0900

    feat. 공통 스타일 변경 적용

--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import s from "./styles/logInPage.module.scss"
+import styles from "./styles/logInPage.module.scss"
 import logo from "../assets/logo/logo.svg"
 import LogInForm from "../components/logInPage/LogInForm";
 import LogInButtons from "../components/logInPage/LogInButtons";
 
 const LogInPage = () => {
     return (
-        <div className={s.container}>
-            <img src={logo} alt="logo" className={s.logo}/>
-            <p className={s.title}>귀여운 고양이들과 함께 하는 오목 한 판!</p>
+        <div className={styles.container}>
+            <img src={logo} alt="logo" className={styles.logo}/>
+            <p className={styles.title}>귀여운 고양이들과 함께 하는 오목 한 판!</p>
             <LogInForm/>
             <LogInButtons/>
         </div>
