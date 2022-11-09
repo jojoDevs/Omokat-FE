4908f8d27de1160b2bf5af464bfb890935e2fc18
Haesol <osunpine@gmail.com>
2022-11-09 18:08:36 +0900

    feat. 메인 반응형 수정

--- src/components/mainPage/Card.tsx
+++ src/components/mainPage/Card.tsx
@@ -1,26 +1,27 @@
 import React from "react";
 import styles from "./styles/card.module.scss";
+import arrow from "../../assets/icons/arrow_right.svg";
 import profile from "../../assets/icons/default_profile.svg";
 
 const Card = () => {
     return (
         <div className={styles.cardWrapper}>
+            <div className={styles.layer}>
+                <p className={styles.message}>
+                    게임방
+                    <br className={styles.hiddenBr}/>
+                    <span>참가하기</span>
+                </p>
+                <img src={arrow} alt="arrow" className={styles.arrow}/>
+            </div>
             {/*<div className={styles.layer}>*/}
-            {/*  <p className={styles.message}>*/}
-            {/*    게임방*/}
-            {/*    <br />*/}
-            {/*    참가하기*/}
-            {/*  </p>*/}
-            {/*  <img src={arrow} alt="arrow" className={styles.arrow} />*/}
-            {/*</div>*/}
-            {/*<div className={styles.layer}>*/}
-            {/*  <p className={styles.message}>*/}
-            {/*    아쉽게도*/}
-            {/*    <br />*/}
-            {/*    게임이 이미*/}
-            {/*    <br />*/}
-            {/*    시작됐어요!*/}
-            {/*  </p>*/}
+            {/*    <p className={styles.message}>*/}
+            {/*        아쉽게도*/}
+            {/*        <br className={styles.hiddenBr}/>*/}
+            {/*        <span>게임이 이미</span>*/}
+            {/*        <br/>*/}
+            {/*        시작됐어요!*/}
+            {/*    </p>*/}
             {/*</div>*/}
             <p className={styles.status}>대기중</p>
             <img src={profile} alt="profile" className={styles.profile}/>
