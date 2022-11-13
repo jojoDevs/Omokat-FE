25cfdebdf15283e70fc433770747959722345e7d
Haesol <osunpine@gmail.com>
2022-11-14 01:10:36 +0900

    feat. 토큰 코드 수정

--- src/shared/routes/instance.ts
+++ src/shared/routes/instance.ts
@@ -1,44 +1,43 @@
-import axios, { AxiosRequestConfig } from "axios";
-import { toNamespacedPath } from "path";
-import { toast } from "react-toastify";
+import axios, {AxiosRequestConfig} from "axios";
+import {toast} from "react-toastify";
 
 const axiosConfig: AxiosRequestConfig = {
-  baseURL: process.env.REACT_APP_SERVER_URL,
+    baseURL: process.env.REACT_APP_SERVER_URL,
 };
 
 const axiosInstance = axios.create(axiosConfig);
 
 axiosInstance.interceptors.request.use(
-  (config: AxiosRequestConfig) => {
-    !config.headers && (config.headers = {});
-    config.headers["Content-Type"] = "application/json; charset=utf-8";
-    config.headers["X-Requested-With"] = "XMLHttpRequest";
-    // config.headers["Authorization"] = null;
-    config.headers.Accept = "application/json";
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
+    (config: AxiosRequestConfig) => {
+        !config.headers && (config.headers = {});
+        config.headers["Content-Type"] = "application/json; charset=utf-8";
+        config.headers["X-Requested-With"] = "XMLHttpRequest";
+        config.headers["Authorization"] = sessionStorage.getItem('token');
+        config.headers.Accept = "application/json";
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
 );
 
 axiosInstance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    const { response } = error;
-    if (response)
-      toast.error(`${Object.values(response.data)[0]}`, {
-        position: "top-center",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-    return Promise.reject(error);
-  }
+    (response) => response,
+    (error) => {
+        const {response} = error;
+        if (response)
+            toast.error(`${Object.values(response.data)[0]}`, {
+                position: "top-center",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+        return Promise.reject(error);
+    }
 );
 
 export default axiosInstance;
