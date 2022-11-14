8be7a160a8861c8190b1fd8d133cd0fd0e775d71
Haesol <osunpine@gmail.com>
2022-11-14 13:27:54 +0900

    feat. api 추가

--- src/shared/routes/apis.ts
+++ src/shared/routes/apis.ts
@@ -7,3 +7,15 @@ export const postSignUp = (id: string, password: string, name: string, answer: s
 // 로그인
 export const postLogIn = (id: string, password: string) =>
     axiosInstance.post(`/login`, {id: id, password: password});
+
+// 아이디 찾기
+export const postFindId = (name: string) =>
+    axiosInstance.post(`/find-id`, {name: name});
+
+// 비밀번호 찾기
+export const postFindPwd = (id: string, answer: string) =>
+    axiosInstance.post(`/find-pwd`, {id: id, answer: answer});
+
+// 비밀번호 재설정
+export const putResetPwd = (id: string, password: string) =>
+    axiosInstance.put(`/reset-pwd`, {id: id, password: password})
