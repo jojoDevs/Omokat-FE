87c9ae613016a67b82a909c652d32aef3f653f01
Haesol <osunpine@gmail.com>
2022-11-14 13:32:09 +0900

    chore. 코드 정리

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import "./App.css";
 import "./shared/styles/buttons.scss";
 import "./shared/styles/inputs.scss";
 import "react-toastify/dist/ReactToastify.css";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
-import { Route, Routes } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+import {QueryClient, QueryClientProvider} from "react-query";
+import {ReactQueryDevtools} from "react-query/devtools";
+import {Route, Routes} from "react-router-dom";
+import {ToastContainer} from "react-toastify";
 import Header from "./components/common/Header";
 import MainPage from "./pages/MainPage";
 import LogInPage from "./pages/LogInPage";
@@ -20,32 +20,32 @@ import EndingContent from "./components/endingPage/EndingContent";
 import GetItem from "./components/endingPage/GetItem";
 
 function App() {
-  const queryClient = new QueryClient();
+    const queryClient = new QueryClient();
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <ToastContainer />
-        <Header />
-        <Routes>
-          <Route path={"/"} element={<MainPage />} />
-          <Route path={"/log-in"} element={<LogInPage />} />
-          <Route path={"/sign-up"} element={<SignUpPage />} />
-          <Route path={"/find-id"} element={<FindIdPage />} />
-          <Route path={"/find-pwd"} element={<FindPwdPage />} />
-          <Route path={"/omok/:roomId"} element={<OmokPage />} />
-          <Route path={"/ending"} element={<EndingPage />}>
-            <Route path={"win"} element={<EndingContent isWin={true} />} />
-            <Route path={"lose"} element={<EndingContent isWin={false} />} />
-            <Route path={"item"} element={<GetItem />} />
-          </Route>
-          <Route path={"/user"} element={<UserPage />} />
-        </Routes>
-        <Footer />
-        <ReactQueryDevtools />
-      </div>
-    </QueryClientProvider>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <div className="App">
+                <ToastContainer/>
+                <Header/>
+                <Routes>
+                    <Route path={"/"} element={<MainPage/>}/>
+                    <Route path={"/log-in"} element={<LogInPage/>}/>
+                    <Route path={"/sign-up"} element={<SignUpPage/>}/>
+                    <Route path={"/find-id"} element={<FindIdPage/>}/>
+                    <Route path={"/find-pwd"} element={<FindPwdPage/>}/>
+                    <Route path={"/omok/:roomId"} element={<OmokPage/>}/>
+                    <Route path={"/ending"} element={<EndingPage/>}>
+                        <Route path={"win"} element={<EndingContent isWin={true}/>}/>
+                        <Route path={"lose"} element={<EndingContent isWin={false}/>}/>
+                        <Route path={"item"} element={<GetItem/>}/>
+                    </Route>
+                    <Route path={"/user"} element={<UserPage/>}/>
+                </Routes>
+                <Footer/>
+                <ReactQueryDevtools/>
+            </div>
+        </QueryClientProvider>
+    );
 }
 
 export default App;
