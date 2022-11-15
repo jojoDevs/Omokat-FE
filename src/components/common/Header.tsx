f9568029f5655b19b0d42702deb908dd8cc0dbf3
seulgiJ <qlenf1560@gmail.com>
2022-11-15 18:36:02 +0900

    feat. 헤더 기능 구현

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,19 +1,36 @@
-import React from "react";
 import styles from "./styles/header.module.scss";
+import { useNavigate } from "react-router-dom";
 import logoBlack from "../../assets/logo/logo_black.svg";
-import profile from "../../assets/icons/default_profile.svg";
-
+import { useEffect } from "react";
 
 const Header = () => {
-    return (
-        <div className={styles.headerContainer}>
-            <div className={styles.headerWrapper}>
-                <img className={styles.headerLogo} src={logoBlack} alt="logo"/>
-                <p className={styles.headerText}>귀여운 고양이들과 함께 하는 오목 한 판!</p>
-                <img className={styles.headerProfile} src={profile} alt="profile"/>
-            </div>
-        </div>
-    );
+  const navigate = useNavigate();
+  let storagePic = sessionStorage.getItem("pic");
+  const profile = storagePic
+    ? storagePic
+    : `${process.env.REACT_APP_S3_URL}default_profile.svg`;
+
+  return (
+    <div className={styles.headerContainer}>
+      <div className={styles.headerWrapper}>
+        <img
+          className={styles.headerLogo}
+          src={logoBlack}
+          alt="logo"
+          onClick={() => navigate("/")}
+        />
+        <p className={styles.headerText}>
+          귀여운 고양이들과 함께 하는 오목 한 판!
+        </p>
+        <img
+          className={styles.headerProfile}
+          src={profile}
+          alt="profile"
+          onClick={() => navigate("/user")}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
