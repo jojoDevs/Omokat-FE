d9b7ccb8a33dd01eab5605848c888d3edccaf8e6
seulgiJ <qlenf1560@gmail.com>
2022-11-15 18:39:34 +0900

    feat. 게임 아이템 객체 생성

---
+++ src/utils/cats.ts
@@ -0,0 +1,64 @@
+const cats = [
+  {
+    id: 0,
+    pic: `${process.env.REACT_APP_S3_URL}/cats/calico.svg`,
+    name: "삼색냥",
+    rate: 10,
+  },
+  {
+    id: 1,
+    pic: `${process.env.REACT_APP_S3_URL}/cats/cheese.svg`,
+    name: "치즈냥",
+    rate: 10,
+  },
+  {
+    id: 2,
+    pic: `${process.env.REACT_APP_S3_URL}/cats/gray_stretch.svg`,
+    name: "회색냥",
+    rate: 10,
+  },
+  {
+    id: 3,
+    pic: `${process.env.REACT_APP_S3_URL}/cats/persian.svg`,
+    name: "페르냥",
+    rate: 10,
+  },
+  {
+    id: 4,
+    pic: `${process.env.REACT_APP_S3_URL}/cats/ragdoll_stretch.svg`,
+    name: "랙돌냥",
+    rate: 10,
+  },
+  {
+    id: 5,
+    pic: `${process.env.REACT_APP_S3_URL}/cats/siamese.svg`,
+    name: "시암냥",
+    rate: 10,
+  },
+  {
+    id: 6,
+    pic: `${process.env.REACT_APP_S3_URL}/cats/tux_sit.svg`,
+    name: "턱시냥",
+    rate: 10,
+  },
+  {
+    id: 7,
+    pic: `${process.env.REACT_APP_S3_URL}/cats/lion.svg`,
+    name: "사자냥",
+    rate: 4,
+  },
+  {
+    id: 8,
+    pic: `${process.env.REACT_APP_S3_URL}/cats/rudolph.svg`,
+    name: "루돌냥",
+    rate: 4,
+  },
+  {
+    id: 9,
+    pic: `${process.env.REACT_APP_S3_URL}/cats/queen.svg`,
+    name: "임금냥",
+    rate: 2,
+  },
+];
+
+export default cats;
